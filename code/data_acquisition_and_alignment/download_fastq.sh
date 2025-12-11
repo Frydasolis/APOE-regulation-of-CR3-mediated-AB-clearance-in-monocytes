5789a3ab5b08218a91c39eb8435110a2b0e63866
Frydasolis <150168784+Frydasolis@users.noreply.github.com>
2025-12-10 20:09:18 -0500

    Remove Seurat processing from download script
    Removed Seurat library loading and data processing steps.

--- code/data_acquisition_and_alignment/download_fastq.sh
+++ code/data_acquisition_and_alignment/download_fastq.sh
@@ -21,23 +21,3 @@ gzip *.fastq
 
 echo "All downloads and compression complete."
 
-library(Seurat)
-library(dplyr)
-
-
-base_dir <- "/datos/lymphocytes/monocytes/ad/fastqfiles"
-
-sample_dirs <- list.dirs(path = base_dir, full.names = TRUE, recursive = FALSE)
-
-seurat_list <- lapply(sample_dirs, function(dir) {
-  sample_name <- basename(dir)                
-  counts <- Read10X(data.dir = dir)          
-  seurat_obj <- CreateSeuratObject(counts = counts, project = sample_name)
-  seurat_obj$Sample <- sample_name           
-  return(seurat_obj)
-})
-
-
-seurat_obj <- merge(seurat_list[[1]], y = seurat_list[-1], add.cell.ids = sapply(seurat_list, function(x) x$Sample))
-saveRDS(seurat_obj, file = "seurat_obj.rds")
-
